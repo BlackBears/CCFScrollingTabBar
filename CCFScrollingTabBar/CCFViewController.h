e51a129a596fc5bfbded6608d8b98be9d9880985
Alan Duncan <alan@cocoa-factory.com>
2012-03-08 14:22:44 -0600

    Initial Commit

---
+++ CCFScrollingTabBar/CCFViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCFViewController.h
+//  CCFScrollingTabBar
+//
+//  Created by Duncan Alan on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFViewController : UIViewController
+
+@end
---
+++ CCFScrollingTabBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CCFScrollingTabBar
+//
+//  Created by Duncan Alan on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCFAppDelegate class]));
+    }
+}
